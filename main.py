58ecba4536b3f1cd318f863921ef05f0c550dbc1
Joseph Rydel <jrydel92@gmail.com>
2023-08-09 15:52:58 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+maker = CoffeeMaker()
+handler = MoneyMachine()
+menu_obj = Menu()
+
+
+on = True
+intro = (f"\nWelcome to the Cyber Cafe!\nWhere coffee is brewed from 1s and 0s!\nWhat would you like? "
+         f"({menu_obj.get_items()}): ")
+while on:
+    selection = input(intro)
+    if selection == "report":
+        maker.report()
+        handler.report()
+    elif selection == "off":
+        on = False
+    else:
+        valid_drink = menu_obj.find_drink(selection)
+        if valid_drink in menu_obj.menu:
+            enough = maker.is_resource_sufficient(valid_drink)
+            if enough:
+                price = valid_drink.cost
+                success = handler.make_payment(price)
+                if success:
+                    maker.make_coffee(valid_drink)
